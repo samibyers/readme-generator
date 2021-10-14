06e556b1e1ef435c759f4cb050bf10d0ce597f3f
Sami Byers <samtaylor.byers@gmail.com>
2021-10-14 11:18:55 -0500

    moved readme setup to the generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,31 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ADD LICENSE BADGE
+  ## Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [license](#license)
+  * [Questions](#questions)
+  ## Description 
+  ${data.description}
+  ## Installation 
+  ${data.installation}
+  ## Usage 
+  ${data.usage}
+  ## Contributing 
+  ${data.contributing}
+  ## Tests 
+  ${data.tests}
+  ## License 
+  ${data.license}
+  ## Questions 
+  If you would like to contact me directly with questions, you can send me an email to: ${data.email}
+  You can find my GitHub profile at: https://github.com/${data.github}
 `;
 }
 
